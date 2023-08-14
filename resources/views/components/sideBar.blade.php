493f25e1933d614e29d0aa33757f3153b864b5ff
Gabriele Rossi <gabriele.rossi000@gmail.com>
2023-08-14 17:43:29 +0200

    check responsive

--- resources/views/components/sideBar.blade.php
+++ resources/views/components/sideBar.blade.php
@@ -1,9 +1,9 @@
 <div id="mySidebar" class="sidebar">
     <div class="subSidebar">
-        <a href="{{route('product.index')}}" class="liNav"><span><i class="material-icons text-dropdown">info</i><span class="text-dropdown icon-text">{{__('ui.sidebarAnnouncements')}}</span></a><br>
+        <a href="{{route('product.index')}}" class="liNav"><span><i class="material-icons text-dropdown">info</i><span class="text-white icon-text">{{__('ui.sidebarAnnouncements')}}</span></a><br>
         <div class="nav-item dropdown">       
             <div>
-                <a class="py-2 pe-3 liNav active text-dropdown dropdown-toggle collapsed" role="button" data-bs-toggle="collapse" data-bs-target="#categoriesDropDown" aria-expanded="false" href="#"><i class="text-dropdown material-icons">search</i><span class="text-dropdown icon-text">{{__('ui.sidebarCategory')}}</span></a><br>
+                <a class="py-2 pe-3 liNav active dropdown-toggle collapsed" role="button" data-bs-toggle="collapse" data-bs-target="#categoriesDropDown" aria-expanded="false" href="#"><i class="text-dropdown material-icons">search</i><span class="text-white icon-text">{{__('ui.sidebarCategory')}}</span></a><br>
                 <ul class="p-0 collapse" id="categoriesDropDown">   
                     @foreach ($categories as $category)
                     <li><a class="dropdown-item text-white subSidebar-content text-center" href="{{route('product.category', compact('category'))}}">{{$category->name}}</a></li>
@@ -15,7 +15,7 @@
         @if(Auth::user()->is_revisor)
         <div class="nav-item dropdown">       
           <div>
-              <a class="py-2 pe-3 liNav active text-dropdown dropdown-toggle collapsed" role="button" data-bs-toggle="collapse" data-bs-target="#categoriesDropDown1" aria-expanded="false" href="#"><i class="text-dropdown material-icons">work</i><span class="text-dropdown icon-text">{{__('ui.spaceRevisor')}}</span></a><br>
+              <a class="py-2 pe-3 liNav active dropdown-toggle text-white collapsed" role="button" data-bs-toggle="collapse" data-bs-target="#categoriesDropDown1" aria-expanded="false" href="#"><i class="text-dropdown material-icons">work</i><span class=" icon-text">{{__('ui.spaceRevisor')}}</span></a><br>
               <ul class="p-0 collapse" id="categoriesDropDown1">   
                   <li><hr class="dropdown-divider"></li>
                   <li><a class="dropdown-item text-white subSidebar-content text-center" href="{{route('revisor.index')}}">{{__('ui.toCheck')}}
